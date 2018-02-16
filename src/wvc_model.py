fbfe5fab06e98f80a2d676437c1da924b95fee38
Rodrigo Santa Cruz <rfsantacruz@gmail.com>
2018-02-16 22:03:48 +1100

    train and val (pre test)

--- src/wvc_model.py
+++ src/wvc_model.py
@@ -1,3 +1,6 @@
+import shutil
+
+import os
 import torch
 import torchvision
 import wvc_model
@@ -34,8 +37,8 @@ def train(train_loader, model, criterion, optimizer, epoch):
         c_loss += loss.data[0]
 
         if i % 100 == 0:
-            _logger.info("Train {}/{}: Loss={}, ACC_1={}, ACC_={}".format(
-                i, total_batches, c_loss / i, c_acc1 / i, c_acc5 / i))
+            _logger.info("Train epoch {}({}/{}): Loss={}, ACC_1={}, ACC_={}".format(
+                epoch, i, total_batches, c_loss / i, c_acc1 / i, c_acc5 / i))
 
 
 def validate(val_loader, model, criterion, epoch):
@@ -59,21 +62,21 @@ def validate(val_loader, model, criterion, epoch):
         c_acc5 += wvc_model.top_k_acc(labels, y_pred.data, top_k=5)
         c_loss += loss.data[0]
         if i % 100 == 0:
-            _logger.info("Validation {}/{}: Loss={}, ACC_1={}, ACC_={}".format(
-                i, total_batches, c_loss / i, c_acc1 / i, c_acc5 / i))
+            _logger.info("Validation {} ({}/{}): Loss={}, ACC_1={}, ACC_={}".format(
+                epoch, i, total_batches, c_loss / i, c_acc1 / i, c_acc5 / i))
 
     return c_loss / total_batches, c_acc1 / total_batches, c_acc5 / total_batches
 
 
 def model_factory(model_name, model_kwargs_dict):
     if model_name == MODELS[0]:
-        torchvision.models.vgg16(pretrained=False, **model_kwargs_dict)
-    if model_name == MODELS[0]:
-        torchvision.models.inception_v3(pretrained=False, **model_kwargs_dict)
-    if model_name == MODELS[0]:
-        torchvision.models.resnet152(pretrained=False, **model_kwargs_dict)
-    if model_name == MODELS[0]:
-        torchvision.models.densenet201(pretrained=False, **model_kwargs_dict)
+        return torchvision.models.vgg16(pretrained=False)
+    if model_name == MODELS[1]:
+        return torchvision.models.inception_v3(pretrained=False)
+    if model_name == MODELS[2]:
+        return torchvision.models.resnet152(pretrained=False)
+    if model_name == MODELS[3]:
+        return torchvision.models.densenet201(pretrained=False)
     else:
         raise ValueError("Model {} is not supported".format(model_name))
 
@@ -86,3 +89,9 @@ def top_k_acc(y_true, y_pred, top_k=5):
     acc = torch.gt(acc, 0).int()
     acc = torch.mean(acc.float())
     return acc
+
+
+def save_checkpoint(state, is_best, ckpt_dir, ckpt_name):
+    torch.save(state, os.path.join(ckpt_dir, ckpt_name))
+    if is_best:
+        shutil.copyfile(os.path.join(ckpt_dir, ckpt_name), os.path.join(ckpt_dir, 'model_best.pth.tar'))
--- src/wvc_train_val.py
+++ src/wvc_train_val.py
@@ -1,39 +1,93 @@
+from torchvision import transforms
+from torch.utils.data import dataloader
+from torch.cuda import device_count
 import torch
-import wvc_model
-import logging
-import torch.utils.data as d
+import wvc_data, wvc_model, wvc_utils
+import logging, os
+
 
 
 _logger = logging.getLogger(__name__)
 
 
-def train(train_loader, model, criterion, optimizer, epoch):
+def main(model_name, output_dir, batch_size=128, num_epochs=100, valid_int=25, checkpoint=None, num_workers=5, kwargs_str=None):
+    # Data loading
+    _logger.info("Reading WebVision Dataset")
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    train_db = wvc_data.WebVision('train', transform=transforms.Compose([transforms.RandomSizedCrop(224),
+                                                                         transforms.RandomHorizontalFlip(),
+                                                                         transforms.ToTensor(), normalize]))
+    train_data_loader = dataloader.DataLoader(train_db, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
 
-    # metrics
-    acc1, acc5, loss, steps = 0.0, 0.0, 0.0, 0.0
-    total_batches = int(len(train_loader.dataset)/train_loader.batch_size)
+    val_db = wvc_data.WebVision('val', transform=transforms.Compose([transforms.CenterCrop(224),
+                                                                     transforms.ToTensor(), normalize]))
+    val_data_loader = dataloader.DataLoader(val_db, batch_size=batch_size, num_workers=num_workers, pin_memory=True)
 
-    # switch to train mode
-    model.train()
-    for i, (images, target) in enumerate(train_loader):
-        target = target.cuda(async=True)
-        image_var = torch.autograd.Variable(images)
-        label_var = torch.autograd.Variable(target)
+    # Model building
+    _logger.info("Building Model: {}".format(model_name))
+    kwargs_dic = wvc_utils.get_kwargs_dic(kwargs_str)
+    _logger.info("Arguments: {}".format(kwargs_dic))
+    model = wvc_model.model_factory(model_name, **kwargs_dic)
+    _logger.info("Parallelizing model in {} GPUS".format(device_count()))
+    model = torch.nn.DataParallel(model).cuda()
+    if checkpoint is not None:
+        _logger.info("Resume training from {}".format(checkpoint))
+        checkpoint = torch.load(checkpoint)
+        start_epoch = checkpoint['epoch']
+        best_acc5 = checkpoint['best_acc5']
+        model.load_state_dict(checkpoint['state_dict'])
+    else:
+        start_epoch, best_acc5 = 0, 0.0
 
-        # compute prediction
-        y_pred = model(image_var)
-        loss = criterion(y_pred, label_var)
+    # Objective and Optimizer
+    _logger.info("Setting up loss function and optimizer")
+    criterion = torch.nn.CrossEntropyLoss().cuda()
+    optimizer = torch.optim.Adam(model.parameters(), **kwargs_dic)
 
-        # compute gradients and backprop
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    # Training and Validation loop
+    _logger.info("Training...")
+    for epoch in range(start_epoch, num_epochs):
+        # train for one epoch
+        wvc_model.train(train_data_loader, model, criterion, optimizer, epoch)
 
-        # measure loss and performance
-        acc1 += wvc_model.top_k_acc(target, y_pred.data, top_k=1)
-        acc5 += wvc_model.top_k_acc(target, y_pred.data, top_k=5)
-        loss += wvc_model.top_k_acc(target, y_pred.data, top_k=5)
+        # Validation
+        if epoch % valid_int == 0:
+            _logger.info("Validating...")
+            val_loss, val_acc1, val_acc5 = wvc_model.validate(val_data_loader, model, criterion, epoch)
+            _logger.info("Epoch {}/{}: val_loss={:.3f}, val_acc1={:.3f}, val_acc5={:.3f}"
+                         .format(epoch, num_epochs, val_loss, val_acc1, val_acc5))
 
-        if i > 100:
-            _logger.info("Batch {}/{}: Loss={}, ACC_1={}, ACC_={}".format(i, total_batches, loss.data[0], acc1, acc5))
+            # save checkpoint
+            model_ckp_name = "M{}_E{}_L{}_ACC1{}_ACC5_{}.pth.tar".format(model_name, epoch, val_loss, val_acc1, val_acc5)
+            _logger.info("Save model checkpoint to {}".format(os.path.join(output_dir, model_ckp_name)))
+            is_best = val_acc5 > best_acc5
+            best_acc5 = max(val_acc5, best_acc5)
+            wvc_model.save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+                'best_acc5': best_acc5}, is_best, output_dir, model_ckp_name)
 
+
+if __name__ == '__main__':
+    import argparse
+    from datetime import datetime
+
+    parser = argparse.ArgumentParser(description="Training and Validation CNN Model.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('model_name', type=str, choices=wvc_model.MODELS, help='Name of the CNN architecture.')
+    parser.add_argument('output_dir', type=str, help='Path to the output directory.')
+    parser.add_argument('-batch_size', type=int, default=128, help='Batch size.')
+    parser.add_argument('-num_epochs', type=int, default=100, help='Number of epochs.')
+    parser.add_argument('-valid_int', type=int, default=25, help='Number epochs between evaluations.')
+    parser.add_argument('-ckp_file', type=str, default=None, help='Resume from checkpoint file.')
+    parser.add_argument('-gpu_str', type=str, default="0", help='Set CUDA_VISIBLE_DEVICES variable.')
+    parser.add_argument('-num_workers', type=int, default=5, help='Number of preprocessing workers.')
+    parser.add_argument('-kwargs_str', type=str, default=None, help="Hyper parameters as string of key value, e.g., k1=v1; k2=v2; ...")
+    args = parser.parse_args()
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_str
+    log_file = os.path.join(args.output_dir, "train_val_{}.log".format(datetime.now().strftime("%Y%m%d-%H%M%S")))
+    wvc_utils.init_logging(log_file)
+    _logger.info("Training and Validation CNN Model Tool: {}".format(args))
+    main(args.model_name, args.output_dir, args.batch_size, args.num_epochs, args.valid_int, args.ckp_file,
+         args.num_workers, args.kwargs_str)
